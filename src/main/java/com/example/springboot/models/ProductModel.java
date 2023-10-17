c78579ec15a305d07a8c9cfca4f0da07ea74fde9
Kauan Meiguins <kauanmeiguins3@gmail.com>
2023-10-17 20:34:33 -0300

    adicionei alguns componentes

--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "TB-PRODUCTS")
-public class productModel implements Serializable {
+public class ProductModel implements Serializable {
     private static final long serialVersionUID =1L;
 
     @Id
